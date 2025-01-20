42bd588ec3d2ae06fa718d2b336ef13e7936d07e
vinoth kumar <vinoth.akithon@gmail.com>
2025-01-20 21:43:44 +0530

    insertion sort using binary search for finding the position instead of using linear search

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -37,6 +37,28 @@ def insertion_sort_recursive_helper(arr, j, i, value) -> None:
         insertion_sort_recursive_helper(arr, i, i+1, arr[i+1])
 
 
+def insertion_sort_using_binary_search(arr: list[int]) -> None:
+    n = len(arr)
+    for i in range(1, n):
+        j = i-1
+        temp = arr[i]
+        pos = binary_search(arr, temp, 0, j)
+        while j >= pos:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            j -= 1
+        arr[j+1] = temp
+
+def binary_search(arr: list[int], target: int, start: int, end: int) -> int:
+    if start > end:
+        return start
+    
+    mid = (start + end) // 2
+    if target < arr[mid]:
+        return binary_search(arr, target, start, mid-1)
+    else:
+        return binary_search(arr, target, mid+1, end)
+
+
 
 class TestCase(unittest.TestCase):
     def test_unsorted_array(self) -> None:
@@ -49,6 +71,9 @@ class TestCase(unittest.TestCase):
         arr = [5,2,10,1,3]
         insertion_sort_recursive(arr)
         self.assertEqual(arr, [1,2,3,5,10])
+        arr = [5,2,10,1,3]
+        insertion_sort_using_binary_search(arr)
+        self.assertEqual(arr, [1,2,3,5,10])
 
     def test_single_element_array(self) -> None:
         arr = [1]
@@ -59,6 +84,9 @@ class TestCase(unittest.TestCase):
         self.assertEqual(arr, [1])
         arr = [1]
         insertion_sort_recursive(arr)
+        self.assertEqual(arr, [1])
+        arr = [1]
+        insertion_sort_using_binary_search(arr)
         self.assertEqual(arr, [1])
 
     def test_empty_array(self) -> None:
@@ -71,6 +99,9 @@ class TestCase(unittest.TestCase):
         arr = []
         insertion_sort_recursive(arr)
         self.assertEqual(arr, [])
+        arr = []
+        insertion_sort_using_binary_search(arr)
+        self.assertEqual(arr, [])
 
     def test_deplicate_elements_array(self) -> None:
         arr = [1,1,1]
@@ -81,6 +112,9 @@ class TestCase(unittest.TestCase):
         self.assertEqual(arr, [1,1,1])
         arr = [1,1,1]
         insertion_sort_recursive(arr)
+        self.assertEqual(arr, [1,1,1])
+        arr = [1,1,1]
+        insertion_sort_using_binary_search(arr)
         self.assertEqual(arr, [1,1,1])
 
     def test_reverse_sorted_array(self) -> None:
@@ -93,6 +127,9 @@ class TestCase(unittest.TestCase):
         arr = [5,4,3,2,1,0]
         insertion_sort_recursive(arr)
         self.assertEqual(arr, [0,1,2,3,4,5])
+        arr = [5,4,3,2,1,0]
+        insertion_sort_using_binary_search(arr)
+        self.assertEqual(arr, [0,1,2,3,4,5])
 
     def test_already_sorted_array(self) -> None:
         arr = [0,1,2,3,4,5]
@@ -103,6 +140,9 @@ class TestCase(unittest.TestCase):
         self.assertEqual(arr, [0,1,2,3,4,5])
         arr = [0,1,2,3,4,5]
         insertion_sort_recursive(arr)
+        self.assertEqual(arr, [0,1,2,3,4,5])
+        arr = [0,1,2,3,4,5]
+        insertion_sort_using_binary_search(arr)
         self.assertEqual(arr, [0,1,2,3,4,5])
 
     
